0a73910545a11cccf9dc0d96412d36f618960125
Fang (MacPro) <faang1980@gmail.com>
2023-12-24 01:55:45 +1300

    update new validator

---
+++ src/MaxWordsRule.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Fstudio\Lvalidator;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class MaxWordsRule implements Rule
+{
+    //define the average length chars for each word
+    const AVERAGE_LENGTH_WORD = 5;
+
+    private $maxWords;
+    private $maxChars;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($_max_words = 500, $_max_chars = null)
+    {
+        $this->maxWords = $_max_words;
+
+        if($_max_chars && is_numeric($_max_chars)){
+            $this->maxChars = $_max_chars;
+        }
+        else{
+            $this->calcMaxChars();
+        }
+
+    }
+
+
+    private function calcMaxChars()
+    {
+        $this->maxChars = $this->maxWords * self::AVERAGE_LENGTH_WORD + ($this->maxWords - 1);
+    }
+
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return str_word_count($value) <= $this->maxWords;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute cannot be more than ' . $this->maxWords . " words (Max Chars [". $this->maxChars ."]).";
+    }
+
+}
