c89fbeac12b14d59b61d2c0332fa1778deee37f3
Fang (MacPro) <faang1980@gmail.com>
2023-12-25 16:17:07 +1300

    add gitignore file

--- src/ReCAPTCHARule.php
+++ src/ReCAPTCHARule.php
@@ -8,14 +8,41 @@ use Fstudio\Recaptcha\ReCAPTCHA;
 class ReCAPTCHARule implements Rule
 {
 
+    private $secret;
+    private $url;
+    private $ip;
+    //default to set IGNORE checking
+    private $ignore;
+
+    public function __construct(String $_secret, String $_url, String $_ip = null, bool $_ignore = false)
+    {
+        $this->secret = $_secret;
+        $this->url    = $_url;
+        $this->ip     = $_ip;
+        $this->ignore = $_ignore;
+    }
+
+
+    public function setIgnore(bool $_ignore)
+    {
+        $this->ignore = $_ignore;
+        return $this;
+    }
+
+
     public function passes($attribute, $value)
     {
-        return true;
+        //If we set ignore this, just RETURN TRUE
+        //Normally used for TESTING on local
+        if($this->ignore)
+            return true;
+
+        return (new ReCAPTCHA($this->secret, $value, $this->url))->verify($this->ip);
     }
 
     public function message()
     {
-        return "";
+        return "The google reCAPTCHA was NOT passed";
     }
 
 }
